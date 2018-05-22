8853394bd527b96e6dbf7bfb9654a8ceddf49994
Mateusz <kaspazza@gmail.com>
2018-05-22 11:34:14 +0200

    upgrade main and diverse some functions

--- main.py
+++ main.py
@@ -1,50 +1,6 @@
-import import_recipes.py
+import imports.py
 import algorithms.py
-
-
-# imports and prints files like welcome.txt end.txt
-def graphics_display(graphic_file):
-    pass
-
-
-#user inputs what he wants to see
-def main_menu_decision():
-    pass
-
-
-def meal_type_decision():
-    choose = input("You want receipts for breakfast, supper or dinner? (b,s,d)")
-
-    if choose == "b":
-        search_type("breakfast")
-
-    elif choose == "s":
-        search_type("supper")
-
-    elif choose == "d":
-        search_type("dinner")
-
-
-def main():
-    graphics_display("welcome.txt")
-    main_menu_decision()
-
-    pass
-
-
-def search_type(file_type):
-
-    choose = input("You want to search for receipts you can make with your \
-    ingredients or receipts containing ingredient?(1 or 2)")
-
-    if choose == "1":
-        dishes = import_recipes.import_txt(file_type)
-        components = pick_components()
-        algorithms.get_dishes(components, dishes)
-
-    elif choose =="2":
-        dishes = import_recipes.import_txt(file_type)
-        components = pick_components()
+import printing.py
 
 
 def pick_components():
@@ -63,6 +19,51 @@ def pick_components():
             components.append(ingredient)
 
     return components
+
+
+def search_type(file_type):
+
+    choose = input("You want to search for receipts you can make with your \
+    ingredients or receipts containing ingredient?(1 or 2)")
+
+    if choose == "1":
+        dishes = import_recipes.import_recipes(file_type)
+        components = pick_components()
+        algorithms.get_dishes(components, dishes)
+
+    elif choose =="2":
+        dishes = import_recipes.import_recipes(file_type)
+        components = pick_components()
+
+
+def meal_type_decision():
+    choose = input("You want receipts for breakfast, supper or dinner? (b,s,d)")
+
+    if choose == "b":
+        search_type("breakfast")
+
+    elif choose == "s":
+        search_type("supper")
+
+    elif choose == "d":
+        search_type("dinner")
+
+
+#user inputs what he wants to see
+def main_menu_decision():
+    decision = "0"
+    while decision != "3":
+        decision = input("what you want to do?")
+        if decision == "1":
+            meal_type_decision()
+        elif decision == "2":
+            printing.graphics_display("authors")
+
+
+def main():
+    printing.graphics_display("welcome.txt")
+    main_menu_decision()
+    printing.graphics_display("end.txt")
 
 
 if __name__ == "__main__":
